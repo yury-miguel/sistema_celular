ee75f3509ec6c41450fb86ed178d54398960e122
yury-miguel <yurymiguelc1@gmail.com>
2024-12-13 19:31:40 -0300

    add pastas

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new ("123456789", "Nokia Antigo", "0000", 48);
+Iphone iphone = new ("987654321", "Iphone 13", "1111", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
